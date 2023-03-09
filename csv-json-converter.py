ddef8d0d91bfe7ac2122d12adc09e00d18a72db7
Priyanka-Gangadhar-Palshetkar <notify.priyanka@gmail.com>
2023-03-09 12:05:51 +0530

    Added json_to_csv converter

--- csv-json-converter.py
+++ csv-json-converter.py
@@ -1,5 +1,6 @@
 import csv 
 import json 
+import pandas as pd
 
 def csv_to_json(csv_file_path, json_file_path):
     json_array = []
@@ -14,11 +15,18 @@ def csv_to_json(csv_file_path, json_file_path):
         json_string = json.dumps(json_array, indent=4)
         jsonf.write(json_string)
 
+def json_to_csv(json_file_path, csv_file_path):
+
+    with open(json_file_path, encoding='utf-8') as f:
+        jsonf = pd.read_json(f)
+    
+    jsonf.to_csv(csv_file_path, encoding='utf-8', index=False)
 
 def main():          
     csv_file_path = r'sample.csv'
     json_file_path = r'sample.json'
     csv_to_json(csv_file_path, json_file_path)
+    json_to_csv(json_file_path, csv_file_path)
 
 
 if __name__ == "__main__":
